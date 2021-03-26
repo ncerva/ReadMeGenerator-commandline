5366ccbe806db2ab1a599f478cc2c85fb4341a80
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-03-26 14:41:56 -0500

    index updated with array

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,50 @@
-// TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+    inquirer
+    .questions([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your project name?',
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter a project description',
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter your installation instructions',
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter usage information',
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter contribution guidelines',
+        },
+        {
+            input: 'input',
+            name: 'name',
+            message: 'Enter test instructions',
+        },
+    ]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile("ReadMe.md", fileName, data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
+uikuy
